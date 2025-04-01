fac2ab3830dfcc8fc59cc3d6e7bfd8d2ffb1cdec
S. Satya Pavan <pavan.simham@gmail.com>
2025-04-01 07:44:42 +0530

    added cpde

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const towers = document.querySelectorAll('.tower');
+let draggedDisk = null;
 
+function createDisks() {
+    const tower1 = document.getElementById('tower1');
+    const diskCount = 5;
+    for (let itr = diskCount; itr > 0; itr--) {
+        const disk = document.createElement('div')
+        disk.classList.add('disk');
+        disk.style.width = `${20 + itr * 20}%`;
+        disk.dataset.size = itr;
+        disk.textContent = itr;
+        tower1.appendChild(disk);
+    }
+}
+
+function initGame() {
+    createDisks();
+}
+
+initGame();
